b487586c0fe2d854ce02fe9a3e8e69b7c7ee7084
JHNewton <h-newton@hotmail.com>
2012-09-13 17:33:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 version = '0.1'
 
 setup(
-	name='ckanext-social',
+	name='ckanext-citation',
 	version=version,
-	description="Social Sharing Plugin",
+	description="Citation Plugin",
 	long_description="""\
 	""",
 	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -14,7 +14,7 @@ setup(
 	url='',
 	license='',
 	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext', 'ckanext.social'],
+	namespace_packages=['ckanext', 'ckanext.citation'],
 	include_package_data=True,
 	zip_safe=False,
 	install_requires=[
@@ -24,6 +24,6 @@ setup(
 	"""
         [ckan.plugins]
 	# Add plugins here, eg
-	social=ckanext.social.plugins:Social
+	citation=ckanext.citation.plugins:Citation
 	""",
 )
